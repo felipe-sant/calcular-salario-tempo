da65e34144c0c9245469a6eead1e6b618816dd23
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-07-22 19:16:25 -0300

    feat: adicionando calculo do salario por mes

--- index.py
+++ index.py
@@ -1,14 +1,18 @@
 from functions.obterFeriados import obterFeriados
 from functions.obterDiasUteis import obterDiasUteis
 from utils.limparTerminal import limparTerminal
+from utils.formatarTexto import formatarTexto_azul, formatarTexto_italico, formatarTexto_negrito
 
-mes = int(input("Digite o mês (MM): "))
-ano = int(input("Digite o ano (YYYY): "))
+salario = float(input(f"Digite o seu {formatarTexto_negrito("salário:")} "))
+mes = int(input(f"Digite o {formatarTexto_negrito("mês")} {formatarTexto_italico("(MM)")}: "))
+ano = int(input(f"Digite o {formatarTexto_negrito("ano")} {formatarTexto_italico("(YYYY)")}: "))
 
 feriados = obterFeriados(ano)
 diasUteis = obterDiasUteis(mes, ano, feriados)
+salario_por_dia = round((salario / diasUteis), 2)
 
-print(diasUteis)
+print("\nDias úteis no mês:", formatarTexto_azul(diasUteis))
+print("Salário por dia:", formatarTexto_azul(salario_por_dia))
 input()
 
 limparTerminal()
---
+++ utils/formatarTexto.py
@@ -0,0 +1,253 @@
+def formatarTexto_negrito(texto):
+    return f"\033[1m{texto}\033[0m"
+
+def formatarTexto_italico(texto):
+    return f"\033[3m{texto}\033[0m"
+
+def formatarTexto_sublinhado(texto):
+    return f"\033[4m{texto}\033[0m"
+
+def formatarTexto_tachado(texto):
+    return f"\033[9m{texto}\033[0m"
+
+def formatarTexto_amarelo(texto):
+    return f"\033[33m{texto}\033[0m"
+
+def formatarTexto_azul(texto):
+    return f"\033[34m{texto}\033[0m"
+
+def formatarTexto_verde(texto):
+    return f"\033[32m{texto}\033[0m"
+
+def formatarTexto_vermelho(texto):
+    return f"\033[31m{texto}\033[0m"
+
+def formatarTexto_ciano(texto):
+    return f"\033[36m{texto}\033[0m"
+
+def formatarTexto_magenta(texto):
+    return f"\033[35m{texto}\033[0m"
+
+def formatarTexto_branco(texto):
+    return f"\033[37m{texto}\033[0m"
+
+def formatarTexto_preto(texto):
+    return f"\033[30m{texto}\033[0m"
+
+def formatarTexto_fundoAmarelo(texto):
+    return f"\033[43m{texto}\033[0m"
+
+def formatarTexto_fundoAzul(texto):
+    return f"\033[44m{texto}\033[0m"
+
+def formatarTexto_fundoVerde(texto):
+    return f"\033[42m{texto}\033[0m"
+
+def formatarTexto_fundoVermelho(texto):
+    return f"\033[41m{texto}\033[0m"
+
+def formatarTexto_fundoCiano(texto):
+    return f"\033[46m{texto}\033[0m"
+
+def formatarTexto_fundoMagenta(texto):
+    return f"\033[45m{texto}\033[0m"
+
+def formatarTexto_fundoBranco(texto):
+    return f"\033[47m{texto}\033[0m"
+
+def formatarTexto_fundoPreto(texto):
+    return f"\033[40m{texto}\033[0m"
+
+def formatarTexto_negrito_amarelo(texto):
+    return f"\033[1;33m{texto}\033[0m"
+
+def formatarTexto_negrito_azul(texto):
+    return f"\033[1;34m{texto}\033[0m"
+
+def formatarTexto_negrito_verde(texto):
+    return f"\033[1;32m{texto}\033[0m"
+
+def formatarTexto_negrito_vermelho(texto):
+    return f"\033[1;31m{texto}\033[0m"
+
+def formatarTexto_negrito_ciano(texto):
+    return f"\033[1;36m{texto}\033[0m"
+
+def formatarTexto_negrito_magenta(texto):
+    return f"\033[1;35m{texto}\033[0m"
+
+def formatarTexto_negrito_branco(texto):
+    return f"\033[1;37m{texto}\033[0m"
+
+def formatarTexto_negrito_preto(texto):
+    return f"\033[1;30m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoAmarelo(texto):
+    return f"\033[1;43m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoAzul(texto):
+    return f"\033[1;44m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoVerde(texto):
+    return f"\033[1;42m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoVermelho(texto):
+    return f"\033[1;41m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoCiano(texto):
+    return f"\033[1;46m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoMagenta(texto):
+    return f"\033[1;45m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoBranco(texto):
+    return f"\033[1;47m{texto}\033[0m"
+
+def formatarTexto_negrito_fundoPreto(texto):
+    return f"\033[1;40m{texto}\033[0m"
+
+def formatarTexto_italico_amarelo(texto):
+    return f"\033[3;33m{texto}\033[0m"
+
+def formatarTexto_italico_azul(texto):
+    return f"\033[3;34m{texto}\033[0m"
+
+def formatarTexto_italico_verde(texto):
+    return f"\033[3;32m{texto}\033[0m"
+
+def formatarTexto_italico_vermelho(texto):
+    return f"\033[3;31m{texto}\033[0m"
+
+def formatarTexto_italico_ciano(texto):
+    return f"\033[3;36m{texto}\033[0m"
+
+def formatarTexto_italico_magenta(texto):
+    return f"\033[3;35m{texto}\033[0m"
+
+def formatarTexto_italico_branco(texto):
+    return f"\033[3;37m{texto}\033[0m"
+
+def formatarTexto_italico_preto(texto):
+    return f"\033[3;30m{texto}\033[0m"
+
+def formatarTexto_italico_fundoAmarelo(texto):
+    return f"\033[3;43m{texto}\033[0m"
+
+def formatarTexto_italico_fundoAzul(texto):
+    return f"\033[3;44m{texto}\033[0m"
+
+def formatarTexto_italico_fundoVerde(texto):
+    return f"\033[3;42m{texto}\033[0m"
+
+def formatarTexto_italico_fundoVermelho(texto):
+    return f"\033[3;41m{texto}\033[0m"
+
+def formatarTexto_italico_fundoCiano(texto):
+    return f"\033[3;46m{texto}\033[0m"
+
+def formatarTexto_italico_fundoMagenta(texto):
+    return f"\033[3;45m{texto}\033[0m"
+
+def formatarTexto_italico_fundoBranco(texto):
+    return f"\033[3;47m{texto}\033[0m"
+
+def formatarTexto_italico_fundoPreto(texto):
+    return f"\033[3;40m{texto}\033[0m"
+
+def formatarTexto_sublinhado_amarelo(texto):
+    return f"\033[4;33m{texto}\033[0m"
+
+def formatarTexto_sublinhado_azul(texto):
+    return f"\033[4;34m{texto}\033[0m"
+
+def formatarTexto_sublinhado_verde(texto):
+    return f"\033[4;32m{texto}\033[0m"
+
+def formatarTexto_sublinhado_vermelho(texto):
+    return f"\033[4;31m{texto}\033[0m"
+
+def formatarTexto_sublinhado_ciano(texto):
+    return f"\033[4;36m{texto}\033[0m"
+
+def formatarTexto_sublinhado_magenta(texto):
+    return f"\033[4;35m{texto}\033[0m"
+
+def formatarTexto_sublinhado_branco(texto):
+    return f"\033[4;37m{texto}\033[0m"
+
+def formatarTexto_sublinhado_preto(texto):
+    return f"\033[4;30m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoAmarelo(texto):
+    return f"\033[4;43m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoAzul(texto):
+    return f"\033[4;44m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoVerde(texto):
+    return f"\033[4;42m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoVermelho(texto):
+    return f"\033[4;41m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoCiano(texto):
+    return f"\033[4;46m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoMagenta(texto):
+    return f"\033[4;45m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoBranco(texto):
+    return f"\033[4;47m{texto}\033[0m"
+
+def formatarTexto_sublinhado_fundoPreto(texto):
+    return f"\033[4;40m{texto}\033[0m"
+
+def formatarTexto_tachado_amarelo(texto):
+    return f"\033[9;33m{texto}\033[0m"
+
+def formatarTexto_tachado_azul(texto):
+    return f"\033[9;34m{texto}\033[0m"
+
+def formatarTexto_tachado_verde(texto):
+    return f"\033[9;32m{texto}\033[0m"
+
+def formatarTexto_tachado_vermelho(texto):
+    return f"\033[9;31m{texto}\033[0m"
+
+def formatarTexto_tachado_ciano(texto):
+    return f"\033[9;36m{texto}\033[0m"
+
+def formatarTexto_tachado_magenta(texto):
+    return f"\033[9;35m{texto}\033[0m"
+
+def formatarTexto_tachado_branco(texto):
+    return f"\033[9;37m{texto}\033[0m"
+
+def formatarTexto_tachado_preto(texto):
+    return f"\033[9;30m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoAmarelo(texto):
+    return f"\033[9;43m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoAzul(texto):
+    return f"\033[9;44m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoVerde(texto):
+    return f"\033[9;42m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoVermelho(texto):
+    return f"\033[9;41m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoCiano(texto):
+    return f"\033[9;46m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoMagenta(texto):
+    return f"\033[9;45m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoBranco(texto):
+    return f"\033[9;47m{texto}\033[0m"
+
+def formatarTexto_tachado_fundoPreto(texto):
+    return f"\033[9;40m{texto}\033[0m"
+
+# Path: utils/formatarTexto.py
